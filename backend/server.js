be0f185b14af35fd1daad9d71185b5dfc8cfe8d5
robitan <robitanjp@gmail.com>
2023-07-11 17:43:47 +0900

    initialize

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => 'Hello, GraphQL!'
+};
+
+const app = express();
+
+app.use(cors());
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true, // Enable GraphiQL for testing the API in the browser
+}));
+
+app.listen(3000, () => {
+  console.log('GraphQL server running at http://localhost:3000/graphql');
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql', // Replace with your server URL
+  cache: new InMemoryCache()
+});
+
+const GET_HELLO = gql`
+  query {
+    hello
+  }
+`;
+
+function App() {
+  const { loading, error, data } = useQuery(GET_HELLO);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>[Error] { error.toString() }</p>;
+
+  return (
+    <div>
+      <h1>{data.hello}</h1>
+    </div>
+  );
+}
+
+function AppWrapper() {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+}
+
+export default AppWrapper;
